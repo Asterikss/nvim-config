26b6984a6f4a57fcc398b80a576d73a035c26298
Andre <AndreAndreM@proton.me>
2022-09-20 08:19:13 +0200

    minimal border added

--- lua/mine/conf-plugs/lsp/init.lua
+++ lua/mine/conf-plugs/lsp/init.lua
@@ -1,7 +1,5 @@
 local lsp = require("lspconfig")
 
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-
 -- :h lsp-handlers		for some reason -v- does not work. I put it in the setup directly
 --[[ vim.lsp.start_client {
 	-- ..., Other configuration omitted.
@@ -13,23 +11,21 @@ local lsp = require("lspconfig")
 		})
 	},
 } ]]
--- put it in handler = handler
 
+-- Might put that like that later - handler = handler ( in .setup)
 -- might not do that on on attach later
-local on_attach = function(_,--[[ _client ,]] bufnr)
+local on_attach = function(_--[[ _client ,]], bufnr)
 	local bufopts = { noremap=true, silent=true, buffer=bufnr }
 	-- -v- Enable completion triggered by <c-x><c-o>
 	-- vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
+	-- -v- see h: deprecated
 	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, bufopts)
-	-- -v- see h: deprecated
-	vim.keymap.set('n', 'gl' , '<cmd>lua vim.diagnostic.open_float()<CR>', bufopts)
-	-- vim.keymap.set('n', 'gl' , '<cmd>lua vim.diagnostic.open_float({ border = "rounded" })<CR>', bufopts)
 	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
 	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
 	vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, bufopts)
@@ -37,18 +33,30 @@ local on_attach = function(_,--[[ _client ,]] bufnr)
 	-- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
 	-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 
+
+	-- an example
+
+		  --[[ local function organize_imports()
+			local params = {
+			  command = 'pyright.organizeimports',
+			  arguments = { vim.uri_from_bufnr(0) },
+			}
+			vim.lsp.buf.execute_command(params)
+		  end ]]
+
+    --[[ if client.name == "pyright" then
+      vim.api.nvim_create_user_command("PyrightOrganizeImports", organize_imports, {desc = 'Organize Imports'})
+    end ]]
+
+	--
+
 end
+
 -- :h lspconfig-setup
 -- :h lspconfig
-
-local lsp_flags = {
-	-- virtual_text = false
-	float = {
-		style = "minimal",
-		header = "",
-		prefix = ""
-	},
-}
+-- :h mason-lspconfig.setup()
+-- :h lspconfig-global-defaults
+-- :h vim.diagnostic.*
 
 lsp['sumneko_lua'].setup{
     on_attach = on_attach,
@@ -58,23 +66,48 @@ lsp['sumneko_lua'].setup{
 			diagnostics = {
 				globals = { "vim" },
 			},
+
+			-- this does not work here (For future reference)
+			--[[ float = {
+				style = "minimal",
+				border = "rounded",
+				header = "",
+				prefix = "",
+			}, ]]
+
 		}
 	},
 
-	lsp_flags = lsp_flags,
+	-- I moved this setup to the bottom of the file (Should be global now)
+	-- Disabling virtual_text works, but I cannot change float specifications
+	-- Works in the vim.diagnostic.config though (down there)
+	-- handlers = {
 
-	handlers = {
-		["textDocument/publishDiagnostics"] = vim.lsp.with(
+		--[[ ["textDocument/publishDiagnostics"] = vim.lsp.with(
 		vim.lsp.diagnostic.on_publish_diagnostics, {
-			virtual_text = false
-		}),
+		-- vim.lsp.diagnostic.config, { --  < this does not work
+			virtual_text = false,
+			-- -v- this does not work. Probably need to be in a different handler
+			float = {
+				style = "minimal",
+				border = "rounded",
+				header = "",
+				prefix = "",
+			}
+		}), ]]
 
-		-- float = {
-		-- 	style = "minimal",
-		-- 	header = "",
-		-- 	prefix = ""
-		-- },
-	}
+		-- this does not work I am not sure what to put inside the ""
+		--[[ ["window/showMessage"] = vim.lsp.with(
+		vim.diagnostic.open_float, {
+			float = {
+				style = "minimal",
+				border = "rounded",
+				header = "",
+				prefix = "",
+			}
+		}), ]]
+
+	-- }
 
 }
 
@@ -82,6 +115,27 @@ lsp['pyright'].setup{
     on_attach = on_attach,
     -- flags = lsp_flags,
 }
+
+-- I didn't override previous vim.diagnostic configs in my testing. Might do that though. .setup does that
+vim.diagnostic.config({
+  virtual_text = false,
+  float = {
+		style = "minimal",
+		border = "rounded",
+		header = "",
+		prefix = "",
+	},
+})
+
+-- does not work (for future reference)
+--[[ vim.diagnostic.open_float.config({
+	float = {
+		style = "minimal",
+		border = "rounded",
+		header = "",
+		prefix = "",
+	}
+}) ]]
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
