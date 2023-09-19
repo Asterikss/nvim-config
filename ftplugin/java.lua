7c40a96df2f3ae519f466580e20144c73cd749d5
Andre <AndreAndreM@proton.me>
2023-09-20 00:44:30 +0200

    ftplugin cleaning

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,8 +1,1 @@
--- https://stackoverflow.com/questions/72984648/neovim-lua-how-to-use-different-mappings-depending-on-file-type 
-local function m(a, b, v)
-	vim.keymap.set(a, b, v, { noremap = true })
-end
--- could be global(from an import)
--- local map = require("utils").map
-
-m("n", "<Leader><A-y>", "YI//<Esc>p")
+vim.api.nvim_buf_set_keymap(0, "n", "<Leader><A-y>", "YI//<Space><Esc>p", { noremap = true })
--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,17 +1,8 @@
--- vim.api.nvim_buf_set_keymap(0, a, b, v, { noremap = true })
--- With nvim_buf_set_keymap everytime a new buffer is opened with .py extension
--- this file will "be sourced again"
--- With normal set it will -> a new buffer with a different file extension, those maps will still work.
--- I am usually working with only one file type during one session
--- vim.api.keymap.set() - https://www.reddit.com/r/neovim/comments/uuh8xw/noob_vimkeymapset_vs_vimapinvim_set_keymap_key/
-
 vim.api.nvim_buf_set_keymap(0, "n", "<Leader><A-y>", "YI#<Space><Esc>p", { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", "qp", "print()<left>", { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", "<a-q>p", 'print("")<left><left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", "qfp", 'print(f"}")<left><left><left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", ";", ':<CR>', { noremap = true })
-
--- https://github.com/EgZvor/vim-black
--- https://github.com/averms/black-nvim/tree/master
+vim.api.nvim_buf_set_keymap(0, "i", 'q;', ';', { noremap = true })
 -- https://github.com/mhartington/formatter.nvim
 -- https://github.com/VonHeikemen/lsp-zero.nvi
