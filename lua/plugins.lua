6c7c34e4a61af1074e9b732fab1f02b070a3dbb2
Andre <AndreAndreM@proton.me>
2023-11-13 18:28:27 +0100

    added setup for neodev

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -106,6 +106,8 @@ require("lazy").setup({
 
 }, {})
 
+require('neodev').setup()
+
 --
 --     -- Unless you are still migrating, remove the deprecated commands from v1.x. run :Neotree
 --     -- vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
