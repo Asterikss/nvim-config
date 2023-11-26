0af67d06f96efc97e8c56fdd183fc9717fce8f88
Andre <AndreAndreM@proton.me>
2023-11-26 01:31:28 +0100

    added LiveGrepGitRoot command

--- lua/cfg/telescope.lua
+++ lua/cfg/telescope.lua
@@ -6,15 +6,8 @@ require("telescope").setup{
 
 			i  = {
 				["<Esc>"] = actions.close,
-				-- For now I will do it like that
-				-- I wanted to use the same key for both i and n
-				-- and continue to switch to insert with <C-j>
-				-- but it stops working with this remap. 
-				-- Can't get to normal now. Can't press ?
-				-- Can't use <C-c> for both.
 				["<C-K>"] = actions.select_default,
 				["<C-h>"] = actions.cycle_history_next,
-				-- -^- or _prev. Used to be <C-k> (or <C-j> idk)
 			},
 
 			n  = {
@@ -23,24 +16,49 @@ require("telescope").setup{
 			},
 
 		},
-
-		-- prompt_prefix = ""
-		-- path_display = { "smart" },
 	}
-
-	--[[ extensions = {
-		fzf = {
-			fuzzy = true,                    -- false will only do exact matching
-			override_generic_sorter = true,  -- override the generic sorter
-			override_file_sorter = true,     -- override the file sorter
-			case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
-		} ]]
 }
 
--- :Telescope find_files cwd=~/.config/ smth or /code 
+-- :Telescope find_files cwd=~/.config/
 
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
-require('telescope').load_extension('fzf')
+pcall(require('telescope').load_extension('fzf'))
 
+
+-- Telescope live_grep in git root
+-- Function to find the git root directory based on the current buffer's path
+local function find_git_root()
+  -- Use the current buffer's path as the starting point for the git search
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_dir
+  local cwd = vim.fn.getcwd()
+  -- If the buffer is not associated with a file, return nil
+  if current_file == "" then
+    current_dir = cwd
+  else
+    -- Extract the directory from the current file's path
+    current_dir = vim.fn.fnamemodify(current_file, ":h")
+  end
+
+  -- Find the Git root directory from the current file's path
+  local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+  if vim.v.shell_error ~= 0 then
+    print("Not a git repository. Searching on current working directory")
+    return cwd
+  end
+  return git_root
+end
+
+
+-- Custom live_grep function to search in git root
+local function live_grep_git_root()
+  local git_root = find_git_root()
+  if git_root then
+    require('telescope.builtin').live_grep({
+      search_dirs = {git_root},
+    })
+  end
+end
+
+vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
