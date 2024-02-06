41e4041dbcd29545081327accb575f92eb38d5b8
Andre <AndreAndreM@proton.me>
2024-02-06 20:42:02 +0100

    Change ; behaviour in python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -2,7 +2,7 @@ vim.api.nvim_buf_set_keymap(0, "n", "<Leader><A-y>", "YI#<Space><Esc>p", { norem
 vim.api.nvim_buf_set_keymap(0, "i", "qp", "print()<left>", { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", "<a-q>p", 'print("")<left><left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", "qfp", 'print(f"}")<left><left><left>', { noremap = true })
-vim.api.nvim_buf_set_keymap(0, "i", ";", ':<CR>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "i", ";", '<Right>:<CR>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", 'q;', ';', { noremap = true })
 -- https://github.com/mhartington/formatter.nvim
 -- https://github.com/VonHeikemen/lsp-zero.nvi
