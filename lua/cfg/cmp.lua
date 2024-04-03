c16ac6e13220a4b711244efdad41a1cf6624c0e8
Andre <AndreAndreM@proton.me>
2024-04-03 22:48:48 +0200

    do some cleanup in cmp.lua

--- lua/cfg/cmp.lua
+++ lua/cfg/cmp.lua
@@ -82,39 +82,18 @@ cmp.setup{
       ['<C-k>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items. clr-y
 	  --["<C-y"] = cmp.config.disable, ]]
 
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(-4),
-                -- ['<C-j>'] = cmp.mapping.,  -- possible danger
-                ['<C-Space>'] = cmp.mapping.complete {},
-                -- ['<C-k>'] = cmp.mapping.confirm {
-                ['<CR>'] = cmp.mapping.confirm {
-                        -- behavior = cmp.ConfirmBehavior.Replace,
-                        behavior = cmp.ConfirmBehavior.Insert,
-                        select = true,
-                },
-                --[[
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                                cmp.select_next_item()
-                        elseif luasnip.expand_or_locally_jumpable() then
-                                luasnip.expand_or_jump()
-                        else
-                                fallback()
-                        end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                                cmp.select_prev_item()
-                        elseif luasnip.locally_jumpable(-1) then
-                                luasnip.jump(-1)
-                        else
-                                fallback()
-                        end
-                end, { 'i', 's' }),
-                ]]
-
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-j>'] = cmp.mapping.,  -- possible danger
+        ['<C-Space>'] = cmp.mapping.complete {},
+        -- ['<C-k>'] = cmp.mapping.confirm {
+        ['<C-y>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert, -- Replace,
+                select = true,
+        },
         },
 
         sources = {
