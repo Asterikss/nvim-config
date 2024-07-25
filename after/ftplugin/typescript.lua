b7110214b46b6f5c9e538699710c482ac37ea1cf
Andre <AndreAndreM@proton.me>
2024-07-25 23:12:13 +0200

    move ftplugin/typescript.lua to after folder to remove 'ro' formatoptions

---
+++ after/ftplugin/typescript.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_buf_set_keymap(0, "i", "qp", 'console.log();<left><left>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "i", "qo", " {}<Left><CR><Esc>O", { noremap = true })
+vim.opt_local.formatoptions:remove({ 'r', 'o' })
+-- To set it globaly paste this inside your config
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "*",
+--   callback = function()
+--     vim.opt_local.formatoptions:remove({ 'r', 'o' })
+--   end,
+-- })
