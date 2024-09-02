c1e6bd742715eb857ad0e0c11745485da63f7af7
Andre <AndreAndreM@proton.me>
2024-09-02 23:52:01 +0200

    add keybinds for typescript

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,5 +1,7 @@
 vim.api.nvim_buf_set_keymap(0, "i", "qp", 'console.log();<left><left>', { noremap = true })
-vim.api.nvim_buf_set_keymap(0, "i", "qo", " {}<Left><CR><Esc>O", { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "i", "qo", "<End> {}<Left><CR><Esc>O", { noremap = true })
+-- vim.api.nvim_buf_set_keymap(0, "i", '"', "'", { noremap = true })
+-- vim.api.nvim_buf_set_keymap(0, "i", "'", '"', { noremap = true })
 vim.opt_local.formatoptions:remove({ 'r', 'o' })
 -- To set it globaly paste this inside your config
 -- vim.api.nvim_create_autocmd("FileType", {
