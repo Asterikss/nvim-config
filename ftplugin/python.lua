199432a4e01eb24bac917064979eafa9f893f09e
Andre <AndreAndreM@proton.me>
2024-09-14 23:24:23 +0200

    change print mappings for python

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_buf_set_keymap(0, "i", "qp", "print()<left>", { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", "<a-q>p", 'print("")<left><left>', { noremap = true })
-vim.api.nvim_buf_set_keymap(0, "i", "qfp", 'print(f"}")<left><left><left>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "i", "qfp", 'print(f"{=}")<left><left><left><left>', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "i", "qffp", 'print(f"}")<left><left><left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", ";", '<End>:<CR>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", 'q;', ';', { noremap = true })
 -- https://github.com/mhartington/formatter.nvim
