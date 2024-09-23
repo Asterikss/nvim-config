2f8eb8c22c03d1e23f4edb05ad9ca82fcbbf78fb
Andre <AndreAndreM@proton.me>
2024-09-23 23:52:36 +0200

    add mapping for commenting all print statements

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,5 +4,6 @@ vim.api.nvim_buf_set_keymap(0, "i", "qfp", 'print(f"{=}")<left><left><left><left
 vim.api.nvim_buf_set_keymap(0, "i", "qffp", 'print(f"}")<left><left><left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", ";", '<End>:<CR>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", 'q;', ';', { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "x", "<Leader>cp", ":g/print/s/^/#/\r", { noremap = true })  -- comment all lines starting with print
 -- https://github.com/mhartington/formatter.nvim
 -- https://github.com/VonHeikemen/lsp-zero.nvi
