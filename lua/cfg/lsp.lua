785f57ac5f3ad19af10f94b7dedd536ba65523cc
Andre <AndreAndreM@proton.me>
2024-09-27 00:51:38 +0200

    take care of lsp mappings

--- lua/cfg/lsp.lua
+++ lua/cfg/lsp.lua
@@ -7,25 +7,19 @@ local on_attach = function(_ --[[ _client ,]], bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    -- nmap('gt', vim.lsp.buf.definition, '[G]oto D[T]efinition')
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame') -- since v0.10.0 grn by default (n)
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction') -- since v0.10.0 gra by default (n, v)
     nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    -- nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences') -- since v0.10.0 grr by default (n)
     nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-    -- nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
     nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<A-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
+    nmap('<A-k>', vim.lsp.buf.signature_help, 'Signature Documentation') -- since v0.10.0 CTRL-S by default (i)
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
     -- Lesser used LSP functionality
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
     -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
     -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
     -- nmap('<leader>wl', function()
