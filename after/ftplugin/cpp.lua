4b5b84e7c0a2c70b559bf2841946d22cfe86613a
Andre <AndreAndreM@proton.me>
2024-11-07 20:33:02 +0100

    move cpp ftplugin to after

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,6 +1,4 @@
 vim.api.nvim_buf_set_option(0, "tabstop", 2)
-vim.api.nvim_buf_set_option(0, "softtabstop", 2)
-vim.api.nvim_buf_set_option(0, "shiftwidth", 2)
 vim.api.nvim_buf_set_keymap(0, "i", "qp", 'std::cout << ;<left>', { noremap = true })
 vim.api.nvim_buf_set_keymap(0, "i", "<a-q>p", 'std::cout << "";<left><left>', { noremap = true })
-vim.api.nvim_buf_set_keymap(0, "i", "q[", " {}<Left><CR><Esc>O", { noremap = true })
+vim.api.nvim_buf_set_keymap(0, "i", "qo", " {}<Left><CR><Esc>O", { noremap = true })
